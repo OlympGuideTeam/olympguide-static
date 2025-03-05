c7f41e8f2bb4df11b63de3e87648d640f063f97c
sundayti <pankratovvlad1@gmail.com>
2025-03-05 23:36:35 +0300

    Create OlympiadRouter.swift

---
+++ iOS/olympguide/Application/Scenes/Olympiads/OlympiadScene/VIP/OlympiadRouter.swift
@@ -0,0 +1,17 @@
+//
+//  OlympiadRouter.swift
+//  olympguide
+//
+//  Created by Tom Tim on 04.03.2025.
+//
+
+import UIKit
+
+final class OlympiadRouter: OlympiadRoutingLogic, OlympiadDataPassing {
+    var dataStore: DataStore?
+    
+    func routeToProgram() {
+        
+    }
+}
+
