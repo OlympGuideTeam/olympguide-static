f19e6b90d127cbc88a0dbbc0cc7f2a9b1981dca6
sundayti <pankratovvlad1@gmail.com>
2025-03-05 23:32:12 +0300

    Create UIStackView+BenefitConfigure.swift

---
+++ iOS/olympguide/Application/Extensions/UIStackView+BenefitConfigure.swift
@@ -0,0 +1,54 @@
+//
+//  UIStackView+BenefitConfigure.swift
+//  olympguide
+//
+//  Created by Tom Tim on 05.03.2025.
+//
+
+import UIKit
+
+extension UIStackView {
+    func configure(
+        with benefit: BenefitsByPrograms.Load.ViewModel.BenefitViewModel.BenefitInformationViewModel
+    ) {
+        self.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        
+        self.distribution = .fillEqually
+        self.spacing = 3
+        self.alignment = .center
+        self.layer.cornerRadius = 8
+        self.backgroundColor = UIColor(hex: "#E0E8FE")
+        self.setHeight(22)
+        
+        let minclassLabel = UILabel()
+        minclassLabel.textColor = .black
+        minclassLabel.font = FontManager.shared.font(weight: .regular, size: 15)
+        minclassLabel.text = "\(benefit.minClass) класс"
+        minclassLabel.layer.cornerRadius = 8
+        minclassLabel.backgroundColor = UIColor(hex: "#E0E8FE")
+        minclassLabel.textAlignment = .center
+        minclassLabel.layer.masksToBounds = true
+        
+        let minDiplomaLevelLabel = UILabel()
+        minDiplomaLevelLabel.textColor = .black
+        minDiplomaLevelLabel.font = FontManager.shared.font(weight: .regular, size: 15)
+        minDiplomaLevelLabel.text = benefit.minDiplomaLevel == 1 ? "Побед." : "Приз."
+        minDiplomaLevelLabel.layer.cornerRadius = 8
+        minDiplomaLevelLabel.backgroundColor = UIColor(hex: "#E0E8FE")
+        minDiplomaLevelLabel.textAlignment = .center
+        minDiplomaLevelLabel.layer.masksToBounds = true
+        
+        let benefitLabel = UILabel()
+        benefitLabel.textColor = .black
+        benefitLabel.font = FontManager.shared.font(weight: .regular, size: 15)
+        benefitLabel.text = benefit.isBVI ? "БВИ" : "100б."
+        benefitLabel.layer.cornerRadius = 8
+        benefitLabel.backgroundColor = UIColor(hex: "#E0E8FE")
+        benefitLabel.textAlignment = .center
+        benefitLabel.layer.masksToBounds = true
+        
+        addArrangedSubview(minclassLabel)
+        addArrangedSubview(minDiplomaLevelLabel)
+        addArrangedSubview(benefitLabel)
+    }
+}
