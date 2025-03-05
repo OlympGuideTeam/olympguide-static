cb999db10fca029562f84a26925d6a3e11fa69b6
sundayti <pankratovvlad1@gmail.com>
2025-03-05 23:33:07 +0300

    Create BenefitsByProgramsModels.swift

---
+++ iOS/olympguide/Application/Scenes/Benefits/BenefitsByProgramsScene/BenefitsByProgramsModels.swift
@@ -0,0 +1,67 @@
+//
+//  BenefitsByProgramsModels.swift
+//  olympguide
+//
+//  Created by Tom Tim on 05.03.2025.
+//
+
+import Foundation
+
+enum BenefitsByPrograms {
+    enum Load {
+        struct Request {
+            let olympiadID: Int
+            let universityID: Int
+            let section: Int
+            var params: [Param] = []
+        }
+        
+        struct Response {
+            var error: Error? = nil
+            var programs: [ProgramWithBenefitsModel]? = nil
+            var section: Int? = nil
+        }
+        
+        struct ViewModel {
+            struct BenefitViewModel {
+                struct Program {
+                    let programID: Int
+                    let programName: String
+                    let field: String
+                }
+                
+                struct BenefitInformationViewModel {
+                    let minClass: Int
+                    let minDiplomaLevel: Int
+                    let isBVI: Bool
+                    
+                    let confirmationSubjects: [Benefit.ConfirmationSubject]?
+                    let fullScoreSubjects: [String]?
+                }
+                
+                let program: Program
+                let benefitInformation: [BenefitInformationViewModel]
+            }
+            
+            let benefits: [BenefitViewModel]
+            let section: Int
+        }
+    }
+}
+
+struct ProgramWithBenefitsModel : Codable {
+    struct Program: Codable {
+        let programID: Int
+        let name: String
+        let field: String
+        let university: String
+        
+        enum CodingKeys: String, CodingKey {
+            case programID = "program_id"
+            case name, field, university
+        }
+    }
+    
+    let program: Program
+    let benefits: [Benefit]
+}
