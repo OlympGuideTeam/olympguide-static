e25c1cda82413f5ea21b7620dc806d07c9b94998
sundayti <pankratovvlad1@gmail.com>
2025-03-05 23:35:10 +0300

    Create Preview.swift

---
+++ iOS/olympguide/Supporting Files/Preview.swift
@@ -0,0 +1,49 @@
+//
+//  Preview.swift
+//  olympguide
+//
+//  Created by Tom Tim on 05.03.2025.
+//
+
+import SwiftUI
+
+struct OlympiadViewControllerWrapper: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> UINavigationController {
+        let olympiadModel = OlympiadModel(
+            olympiadID: 279,
+            name: "Отраслевая физико-математическая олимпиада школьников «Росатом»",
+            level: 2,
+            profile: "математика",
+            like: false
+        )
+        let vc = OlympiadAssembly.build(with: olympiadModel)
+        
+        return NavigationBarViewController(rootViewController: vc)
+    }
+    
+    
+    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {}
+}
+
+struct FieldViewControllerWrapper: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> UINavigationController {
+        let field = GroupOfFieldsModel.FieldModel(
+            fieldId: 1,
+            name: "Программная инженерия и бла бла бла бла чтоб название подлиннее было",
+            code: "09.03.04",
+            degree: "Бакалавриат"
+        )
+        
+        let vc = FieldViewController(for: field)
+        
+        return NavigationBarViewController(rootViewController: vc)
+    }
+    
+    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {}
+}
+
+#Preview {
+    OlympiadViewControllerWrapper()
+}
