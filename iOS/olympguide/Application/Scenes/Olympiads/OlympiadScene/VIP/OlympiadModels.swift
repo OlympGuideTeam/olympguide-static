c9a4078d53417148258f178a7c2c8716a0ba0447
sundayti <pankratovvlad1@gmail.com>
2025-03-05 23:36:43 +0300

    Update OlympiadModels.swift

--- iOS/olympguide/Application/Scenes/Olympiads/OlympiadScene/VIP/OlympiadModels.swift
+++ iOS/olympguide/Application/Scenes/Olympiads/OlympiadScene/VIP/OlympiadModels.swift
@@ -5,4 +5,27 @@
 //  Created by Tom Tim on 01.03.2025.
 //
 
-import Foundation
+enum Olympiad {
+    enum LoadUniversities {
+        struct Request {
+            let olympiadID: Int
+        }
+        
+        struct Response {
+            var universities: [UniversityModel]? = nil
+            var error: Error? = nil
+        }
+        
+        struct ViewModel {
+            let universities: [UniversityViewModel]
+        }
+    }
+}
+
+struct Model : Codable {
+    //    let olympiadID: Int
+    //    enum CodingKeys: String, CodingKey {
+    //        case olympiadID = "olympiad_id"
+    //        case name, profile, level, like
+    //    }
+}
