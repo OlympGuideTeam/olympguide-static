9b805b086dd6e8945c6e392e6fdf0aa61916e480
sundayti <pankratovvlad1@gmail.com>
2025-03-06 15:49:09 +0300

    Update FieldInteractor.swift

--- iOS/olympguide/Application/Scenes/Fields/FieldScene/VIP/FieldInteractor.swift
+++ iOS/olympguide/Application/Scenes/Fields/FieldScene/VIP/FieldInteractor.swift
@@ -5,12 +5,26 @@
 //  Created by Tom Tim on 04.03.2025.
 //
 
-final class FieldInteractor: FieldBusinessLogic {
+final class FieldInteractor: FieldBusinessLogic, FieldDataStore {
     var presenter: FieldPresentationLogic?
-    var worker: FieldWorkerLogic = FieldWorker()
+    var worker: FieldWorkerLogic?
+    var programs: [ProgramsByUniversityModel]?
     
     func loadPrograms(with request: Field.LoadPrograms.Request) {
-        
+        worker?.fetchPrograms(
+            for: request.fieldID,
+            with: request.params
+        ) { [weak self] result in
+            switch result {
+            case .success(let programs):
+                self?.programs = programs
+                let response = Field.LoadPrograms.Response(programs: programs)
+                self?.presenter?.presentLoadPrograms(with: response)
+            case .failure(let error):
+                let response = Field.LoadPrograms.Response(error: error)
+                self?.presenter?.presentLoadPrograms(with: response)
+            }
+        }
     }
 }
 
