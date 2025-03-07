ee1ee403467ea5c2c77aa8437b294c181ce1a2be
sundayti <pankratovvlad1@gmail.com>
2025-03-07 23:48:01 +0300

    Update FavoriteProgramsInteractor.swift

--- iOS/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsInteractor.swift
+++ iOS/olympguide/Application/Scenes/ProgramsScenes/FavoriteProgramsScene/VIP/FavoriteProgramsInteractor.swift
@@ -5,9 +5,12 @@
 //  Created by Tom Tim on 27.02.2025.
 //
 
+import Foundation
+
 final class FavoriteProgramsInteractor: FavoriteProgramsBusinessLogic, FavoriteProgramsDataStore {
-    var programs: [ProgramModel] = []
-    var removePrograms: [Int: ProgramModel] = [:]
+    var programs: [ProgramsByUniversityModel] = []
+    var removedPrograms: [Int: (UniversityModel, ProgramShortModel)] = [:]
+    
     var presenter: FavoriteProgramsPresentationLogic?
     var worker: FavoriteProgramsWorkerLogic?
     
@@ -18,12 +21,9 @@ final class FavoriteProgramsInteractor: FavoriteProgramsBusinessLogic, FavoriteP
             case .success(let programs):
                 self?.programs =  programs.map { program in
                     var modifiedPrograms = program
-                    modifiedPrograms.like = self?.isFavorite(
-                        programID: program.programID,
-                        serverValue: program.like
-                    ) ?? false
+                    modifiedPrograms.programs = modifiedPrograms.programs.filter {$0.like}
                     return modifiedPrograms
-                }.filter { $0.like }
+                }
                 self?.presenter?.presentLoadPrograms(with: FavoritePrograms.Load.Response(programs: programs))
             case .failure(let error):
                 self?.presenter?.presentLoadPrograms(with: FavoritePrograms.Load.Response(error: error))
@@ -31,24 +31,49 @@ final class FavoriteProgramsInteractor: FavoriteProgramsBusinessLogic, FavoriteP
         }
     }
     
-    func likeProgram(_ program: ProgramModel, at insertIndex: Int) {
-        programs.insert(program, at: insertIndex)
-        removePrograms[program.programID] = nil
+    func likeProgram(_ univer: UniversityModel, _ program: ProgramShortModel) {
+        if let insertSection = programs.firstIndex(where: { $0.univer.universityID == univer.universityID}) {
+            let insertRaw = programs[insertSection].programs.firstIndex(where: { $0.programID > program.programID}) ?? programs[insertSection].programs.count
+            programs[insertSection].programs.insert(program, at: insertRaw)
+        } else {
+            let insertSection = programs.firstIndex { $0.univer.universityID > univer.universityID} ?? self.programs.count
+            let newElement = ProgramsByUniversityModel(
+                univer: univer,
+                programs: [program]
+            )
+            programs.insert(newElement, at: insertSection)
+        }
+        removedPrograms[program.programID] = nil
+        
+        let response = FavoritePrograms.Load.Response(programs: programs)
+        presenter?.presentLoadPrograms(with: response)
     }
     
-    func dislikeProgram(at index: Int) {
-        let program = programs[index]
-        removePrograms[program.programID] = program
-        programs.remove(at: index)
+    func dislikeProgram(at indexPath: IndexPath) {
+        let program = programs[indexPath.section].programs.remove(at: indexPath.row)
+        let univer = programs[indexPath.section].univer
+        removedPrograms[program.programID] = (univer, program)
+        if programs[indexPath.section].programs.isEmpty {
+            programs.remove(at: indexPath.section)
+        }
     }
     
     func handleBatchError(programID: Int) {
-        if let program = removePrograms[programID] {
-            let insertIndex = programs.firstIndex { $0.programID > program.programID } ?? self.programs.count
-            programs.insert(program, at: insertIndex)
+        if let (univer, program) = removedPrograms[programID] {
+            if let insertSection = programs.firstIndex(where: { $0.univer.universityID == univer.universityID}) {
+                let insertRaw = programs[insertSection].programs.firstIndex(where: { $0.programID > program.programID}) ?? programs[insertSection].programs.count
+                programs[insertSection].programs.insert(program, at: insertRaw)
+            } else {
+                let insertSection = programs.firstIndex { $0.univer.universityID > univer.universityID} ?? self.programs.count
+                let newElement = ProgramsByUniversityModel(
+                    univer: univer,
+                    programs: [program]
+                )
+                programs.insert(newElement, at: insertSection)
+            }
         } else {
-            if let index = programs.firstIndex(where: { $0.programID == programID }) {
-                programs.remove(at: index)
+            if let index = findIndexPath(programID: programID) {
+                programs[index.section].programs.remove(at: index.row)
             }
         }
         
@@ -56,14 +81,25 @@ final class FavoriteProgramsInteractor: FavoriteProgramsBusinessLogic, FavoriteP
         presenter?.presentLoadPrograms(with: response)
     }
     
+    private func findIndexPath(programID: Int) -> IndexPath? {
+        for (section, program) in programs.enumerated() {
+            if let index = program.programs.firstIndex(where: {
+                $0.programID == programID
+            }) {
+                return IndexPath(row: index, section: section)
+            }
+        }
+        return nil
+    }
+    
     func handleBatchSuccess(programID: Int, isFavorite: Bool) {
         if !isFavorite {
-            removePrograms[programID] = nil
+            removedPrograms[programID] = nil
         }
     }
     
-    func restoreFavorite(at index: Int) -> Bool {
-        programs[index].like
+    func restoreFavorite(at indexPath: IndexPath) -> Bool {
+        programs[indexPath.section].programs[indexPath.row].like
     }
     
     func isFavorite(programID: Int, serverValue: Bool) -> Bool {
