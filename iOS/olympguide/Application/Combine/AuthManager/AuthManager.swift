a8fefb085fa8298e9d01747076e0bae76de809a1
sundayti <pankratovvlad1@gmail.com>
2025-03-14 16:24:52 +0300

    Move AuthManagerProtocol

--- iOS/olympguide/Application/Combine/AuthManager/AuthManager.swift
+++ iOS/olympguide/Application/Combine/AuthManager/AuthManager.swift
@@ -8,21 +8,6 @@
 import Foundation
 import Combine
 
-protocol AuthManagerProtocol {
-    var isAuthenticatedPublisher: AnyPublisher<Bool, Never> { get }
-    var isAuthenticated: Bool { get }
-    
-    func login(
-        email: String,
-        password: String,
-        completion: @escaping (Result<BaseServerResponse, NetworkError>) -> Void
-    )
-    
-    func checkSession()
-    
-    func logout(completion: ((Result<BaseServerResponse, NetworkError>) -> Void)?)
-}
-
 class AuthManager : AuthManagerProtocol {
     static let shared = AuthManager()
     
