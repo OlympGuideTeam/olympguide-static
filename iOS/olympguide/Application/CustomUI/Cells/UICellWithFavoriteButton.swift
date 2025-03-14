6161f15df7155f3ae9d851563ef7a38cfc17578e
sundayti <pankratovvlad1@gmail.com>
2025-03-14 16:45:26 +0300

    Update UICellWithFavoriteButton.swift
    Add configureFavoriteButton instead of initializer

--- iOS/olympguide/Application/CustomUI/Cells/UICellWithFavoriteButton.swift
+++ iOS/olympguide/Application/CustomUI/Cells/UICellWithFavoriteButton.swift
@@ -9,26 +9,28 @@ import UIKit
 import Combine
 
 class UICellWithFavoriteButton: UITableViewCell {
+    typealias Common = AllConstants.Common
+
     @InjectSingleton
     var authManager: AuthManagerProtocol
     
     private var cancellables = Set<AnyCancellable>()
     var isFavoriteButtonHidden: Bool = false
     var favoriteButtonTapped: ((_: UIButton, _: Bool) -> Void)?
-    let favoriteButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.tintColor = .black
-        button.contentHorizontalAlignment = .fill
-        button.contentVerticalAlignment = .fill
-        button.imageView?.contentMode = .scaleAspectFit
-        button.setImage(UIImage(systemName: "bookmark"), for: .normal)
-        return button
-    }()
+    let favoriteButton: UIButton = UIButton()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureFavoriteButton()
         setupBindings()
+    }
+    
+    private func configureFavoriteButton() {
+        favoriteButton.tintColor = .black
+        favoriteButton.contentHorizontalAlignment = .fill
+        favoriteButton.contentVerticalAlignment = .fill
+        favoriteButton.imageView?.contentMode = .scaleAspectFit
+        favoriteButton.setImage(Common.Images.unlike, for: .normal)
     }
     
     @available(*, unavailable)
